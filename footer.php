ee182dce1aad94181f119ae2fead9a4dea3214a9
liliang <liliang0310@gmail.com>
2012-11-07 21:33:09 +0800

    add

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <div id="footer">
-	<p>Powered by <a href="http://www.wordpress.org">WordPress</a> &#124; <<a href="http://liliang13.com">icoffee</a> Theme
+	<p>Powered by <a href="http://www.wordpress.org">WordPress</a> &#124; <a href="http://liliang13.com">icoffee</a> Theme
 </div>
