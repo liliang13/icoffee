c01bdde9a0402ba824da68eafe85910850f6bad2
liliang <liliang0310@gmail.com>
2012-11-07 21:25:50 +0800

    add

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+
+
+<div id="posts">
+
+<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+
+<div class="post" id="post-<?php the_ID(); ?>">
+
+<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+
+<div class="date">
+<?php the_time('Y-n-d H:i:s') ?> 
+<?php edit_post_link('Edit', ' &#124; ', ''); ?>
+</div>
+<div class="entry">
+
+<?php the_content(); ?>
+
+<!-- <div class="postmetadata">
+<?php _e('分类:'); ?>
+<?php the_category(', ') ?>
+</div> -->
+<div class="tags">
+<?php the_tags('标签: ', ', ', ''); ?>
+</div>
+</div>
+
+</div>
+
+<?php endwhile; ?>
+
+<div class="navigation">
+<?php posts_nav_link(' | ','&laquo; Previous Page','Next Page &raquo;'); ?>
+</div>
+
+<?php else : ?>
+
+<div class="post" id="post-<?php the_ID(); ?>">
+<h2><?php _e('Not Found'); ?></h2>
+</div>
+
+<?php endif; ?>
+
+</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
+
+<?php wp_footer(); ?>
+
+</div>
+
+</body>
+</html>
